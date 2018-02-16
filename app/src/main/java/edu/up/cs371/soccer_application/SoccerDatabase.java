dcb9af56ba3919383e7c1fea4053434b9aada653
Voltrage <ldanowskion@gmail.com>
2018-02-15 18:31:28 -0800

    finished

--- app/src/main/java/edu/up/cs371/soccer_application/SoccerDatabase.java
+++ app/src/main/java/edu/up/cs371/soccer_application/SoccerDatabase.java
@@ -2,21 +2,29 @@ package edu.up.cs371.soccer_application;
 
 import android.util.Log;
 
-import edu.up.cs371.soccer_application.soccerPlayer.SoccerPlayer;
-
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.*;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Hashtable;
+
+import edu.up.cs371.soccer_application.soccerPlayer.SoccerPlayer;
 
 /**
  * Soccer player database -- presently, all dummied up
- * 
+ *
  * @author *** put your name here ***
  * @version *** put date of completion here ***
- *
  */
 public class SoccerDatabase implements SoccerDB {
+
+
+    Hashtable<String, SoccerPlayer> playerHashtable = new Hashtable<String, SoccerPlayer>();
 
     /**
      * add a player
@@ -24,10 +32,17 @@ public class SoccerDatabase implements SoccerDB {
      * @see SoccerDB#addPlayer(String, String, int, String)
      */
     @Override
-	public boolean addPlayer(String firstName, String lastName,
-			int uniformNumber, String teamName) {
-        return false;
-	}
+    public boolean addPlayer(String firstName, String lastName,
+                             int uniformNumber, String teamName) {
+
+        if (playerHashtable.containsKey(firstName + lastName)) {
+            return false;
+        } else {
+            SoccerPlayer nextPlayer = new SoccerPlayer(firstName, lastName, uniformNumber, teamName);
+            playerHashtable.put(firstName + lastName, nextPlayer);
+        }
+        return true;
+    }
 
     /**
      * remove a player
@@ -36,7 +51,11 @@ public class SoccerDatabase implements SoccerDB {
      */
     @Override
     public boolean removePlayer(String firstName, String lastName) {
-        return false;
+        if (playerHashtable.containsKey(firstName + lastName)) {
+            playerHashtable.remove(firstName + lastName);
+            return true;
+        } else
+            return false;
     }
 
     /**
@@ -45,8 +64,11 @@ public class SoccerDatabase implements SoccerDB {
      * @see SoccerDB#getPlayer(String, String)
      */
     @Override
-	public SoccerPlayer getPlayer(String firstName, String lastName) {
-        return null;
+    public SoccerPlayer getPlayer(String firstName, String lastName) {
+        if (playerHashtable.containsKey(firstName + lastName)) {
+            return playerHashtable.get(firstName + lastName);
+        } else
+            return null;
     }
 
     /**
@@ -56,7 +78,11 @@ public class SoccerDatabase implements SoccerDB {
      */
     @Override
     public boolean bumpGoals(String firstName, String lastName) {
-        return false;
+        if (playerHashtable.containsKey(firstName + lastName)) {
+            getPlayer(firstName, lastName).bumpGoals();
+            return true;
+        } else
+            return false;
     }
 
     /**
@@ -66,7 +92,13 @@ public class SoccerDatabase implements SoccerDB {
      */
     @Override
     public boolean bumpAssists(String firstName, String lastName) {
-        return false;
+        if (playerHashtable.containsKey(firstName + lastName)) {
+            getPlayer(firstName, lastName).bumpAssists();
+            return true;
+        } else {
+            return false;
+        }
+
     }
 
     /**
@@ -76,7 +108,12 @@ public class SoccerDatabase implements SoccerDB {
      */
     @Override
     public boolean bumpShots(String firstName, String lastName) {
-        return false;
+        if (playerHashtable.containsKey(firstName + lastName)) {
+            getPlayer(firstName, lastName).bumpShots();
+            return true;
+        } else {
+            return false;
+        }
     }
 
     /**
@@ -86,7 +123,12 @@ public class SoccerDatabase implements SoccerDB {
      */
     @Override
     public boolean bumpSaves(String firstName, String lastName) {
-        return false;
+        if (playerHashtable.containsKey(firstName + lastName)) {
+            getPlayer(firstName, lastName).bumpSaves();
+            return true;
+        } else {
+            return false;
+        }
     }
 
     /**
@@ -96,7 +138,12 @@ public class SoccerDatabase implements SoccerDB {
      */
     @Override
     public boolean bumpFouls(String firstName, String lastName) {
-        return false;
+        if (playerHashtable.containsKey(firstName + lastName)) {
+            getPlayer(firstName, lastName).bumpFouls();
+            return true;
+        } else {
+            return false;
+        }
     }
 
     /**
@@ -106,7 +153,12 @@ public class SoccerDatabase implements SoccerDB {
      */
     @Override
     public boolean bumpYellowCards(String firstName, String lastName) {
-        return false;
+        if (playerHashtable.containsKey(firstName + lastName)) {
+            getPlayer(firstName, lastName).bumpYellowCards();
+            return true;
+        } else {
+            return false;
+        }
     }
 
     /**
@@ -116,7 +168,12 @@ public class SoccerDatabase implements SoccerDB {
      */
     @Override
     public boolean bumpRedCards(String firstName, String lastName) {
-        return false;
+        if (playerHashtable.containsKey(firstName + lastName)) {
+            getPlayer(firstName, lastName).bumpRedCards();
+            return true;
+        } else {
+            return false;
+        }
     }
 
     /**
@@ -126,19 +183,42 @@ public class SoccerDatabase implements SoccerDB {
      */
     @Override
     // report number of players on a given team (or all players, if null)
-	public int numPlayers(String teamName) {
-        return -1;
-	}
+    public int numPlayers(String teamName) {
+
+        int count = 0;
+        Enumeration<String> enumKey = playerHashtable.keys();
+
+        while (enumKey.hasMoreElements()) {
+
+            String key = enumKey.nextElement();
+            SoccerPlayer player = playerHashtable.get(key);
+            if (player.getTeamName().equals(teamName))
+                count++;
+        }
+        return count;
+    }
 
     /**
      * gives the nth player on a the given team
      *
      * @see SoccerDB#playerNum(int, String)
      */
-	// get the nTH player
-	@Override
+    // get the nTH player
+    @Override
     public SoccerPlayer playerNum(int idx, String teamName) {
-        return null;
+        int count;
+        SoccerPlayer player=null;
+        Enumeration<String> enumKey = playerHashtable.keys();
+
+        for(count=idx ; count>=0 ; count--){
+            if(enumKey.hasMoreElements()) {
+                String key = enumKey.nextElement();
+                player = playerHashtable.get(key);
+            }
+            else
+                return null;
+        }
+        return player;
     }
 
     /**
@@ -146,25 +226,78 @@ public class SoccerDatabase implements SoccerDB {
      *
      * @see SoccerDB#readData(java.io.File)
      */
-	// read data from file
+    // read data from file
     @Override
-	public boolean readData(File file) {
-        return file.exists();
-	}
+    public boolean readData(File file) {
+
+        String line;
+        try {
+            FileReader reader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(reader);
+
+
+
+            while((line = bufferedReader.readLine()) != null){
+                logString(line);
+            }
+
+            bufferedReader.close();
+            reader.close();
+            return true;
+
+        } catch (IOException e) {
+            return file.exists();
+        }
+    }
 
     /**
      * write database data to a file
      *
      * @see SoccerDB#writeData(java.io.File)
      */
-	// write data to file
+    // write data to file
     @Override
-	public boolean writeData(File file) {
-        return false;
-	}
+    public boolean writeData(File file) {
+
+
+        try {
+
+            Enumeration<String> enumKey = playerHashtable.keys();
+            SoccerPlayer player;
+            PrintWriter writer = new PrintWriter(file);
+
+            while(enumKey.hasMoreElements()){
+
+                player = playerHashtable.get(enumKey.nextElement());
+
+                writer.println("Player: " + player.getFirstName() + " " + player.getLastName() + "\n" +
+                        "Uniform Number: " + player.getUniform() + "\n" +
+                        "on Team: " + player.getTeamName() + "\n" +
+                        "Goals: " + player.getGoals() + "\n" +
+                        "Assists: " + player.getAssists() + "'\n" +
+                        "Shots: " + player.getShots() + "\n" +
+                        "Saves: " + player.getSaves() + "\n" +
+                        "Fouls: " + player.getFouls() + "\n" +
+                        "Yellow Cards: " + player.getYellowCards() + "\n" +
+                        "Red Cards: " + player.getRedCards() + "\n\n");
+
+//                enumKey.nextElement();
+
+            }
+
+            writer.close();
+
+            return true;
+
+        } catch (IOException e) {
+            return false;
+        }
+
+    }
 
     /**
      * helper method that logcat-logs a string, and then returns the string.
+     *
      * @param s the string to log
      * @return the string s, unchanged
      */
@@ -178,10 +311,11 @@ public class SoccerDatabase implements SoccerDB {
      *
      * @see edu.up.cs371.soccer_application.SoccerDB#getTeams()
      */
-	// return list of teams
+    // return list of teams
     @Override
-	public HashSet<String> getTeams() {
+    public HashSet<String> getTeams() {
+
         return new HashSet<String>();
-	}
+    }
 
 }
